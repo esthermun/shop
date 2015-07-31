056b20e6b0d0262080e45a1aed42ddcdbb612790
esthermun <esther_mun@hotmail.com>
2015-07-31 01:57:51 -0400

    Revert c7df761..ae4e288
    This rolls back to commit c7df7611124e0c73b69251f15d5c5c2a72682e37.

--- app/models/admin.rb
+++
@@ -1,6 +0,0 @@
-class Admin < ActiveRecord::Base
-  # Include default devise modules. Others available are:
-  # :confirmable, :recoverable, :rememberable, :lockable, :timeoutable and :omniauthable, :registerable,
-  devise :database_authenticatable, :lockable,
-          :trackable, :validatable
-end
